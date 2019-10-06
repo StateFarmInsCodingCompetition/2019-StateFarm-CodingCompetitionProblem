e0d546fe8d4b33d0096c6abcc132a67d93ec1221
Sidhartha Chaganti <chagantisidhartha@gmail.com>
2019-10-05 23:52:27 -0400

    Added documentation to source files

--- src/main/java/codingcompetition2019/CodingCompCSVUtil.java
+++ src/main/java/codingcompetition2019/CodingCompCSVUtil.java
@@ -6,9 +6,19 @@ import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * A class holding methods to analyze csv files of disasters
+ * @author Sidhartha Chaganti, schaganti@gatech.edu
+ * @version 1.0.0
+ */
 public class CodingCompCSVUtil {
+        /**
+         * Reads a specified CSV file and returns only the entries pertaining to a specified country
+         * @param fileName The csv file to be read
+         * @param countryName The name of the country with the disasters we are searching for
+         * @return A List of List of Strings containing the entries of the disasters from a particular country
+         */
 	public List<List<String>> readCSVFileByCountry(String fileName, String countryName) throws IOException {
-		// TODO implement this method
                 List<List<String>> finalList = new ArrayList<List<String>>();
                 BufferedReader csvReader = new BufferedReader(new FileReader(fileName));
                 String row = csvReader.readLine();
@@ -27,6 +37,11 @@ public class CodingCompCSVUtil {
 		return finalList;
 	}
 	
+        /**
+         * Reads a CSV file and returns all entries 
+         * @param fileName The csv file to be read
+         * @return A List of List of Strings of all entries from the csv file
+         */
 	public List<List<String>> readCSVFileWithHeaders(String fileName) throws IOException {
 		// TODO implement this method
                 List<List<String>> finalList = new ArrayList<List<String>>();
@@ -45,6 +60,11 @@ public class CodingCompCSVUtil {
 		return finalList;
 	}
 	
+        /**
+         * Reads a CSV file and returns all entries except for the header entry
+         * @param fileName The csv file to be read
+         * @return A List of List of Strings of all entries from the csv file except the header entry
+         */
 	public List<List<String>> readCSVFileWithoutHeaders(String fileName) throws IOException {
 		// TODO implement this method
                 List<List<String>> finalList = new ArrayList<List<String>>();
@@ -63,7 +83,11 @@ public class CodingCompCSVUtil {
 		return finalList;
 	}
 	
-        // entity, code, year, numIncidents
+        /**
+         * Finds and returns the DisasterDescriptionInstance of the record entry with the most impactful year (the year with the most incidents)
+         * @param records The List of List of Strings containing the disaster entries
+         * @return a DisasterDescription instance of the year with the most disaster incidents
+         */
 	public DisasterDescription getMostImpactfulYear(List<List<String>> records) {
             // TODO implement this method
             // let first entry be the most impactful
@@ -79,6 +103,12 @@ public class CodingCompCSVUtil {
 		return mostImpactfulDisasterDescription;
 	}
 
+        /**
+         * Finds and returns the DisasterDescriptionInstance of the record entry with the most impactful year (the year with the most incidents) and the specified category
+         * @param category The category of disaster to filter records by
+         * @param records The List of List of Strings containing the disaster entries
+         * @return a DisasterDescription instance of the year with the most disaster incidents and the specified category
+         */
 	public DisasterDescription getMostImpactfulYearByCategory(String category, List<List<String>> records) {
             // TODO implement this method
             // need to find the first instance of the category we want
@@ -93,7 +123,12 @@ public class CodingCompCSVUtil {
 		return mostImpactfulDisasterDescription;
 	}
 
-        // entity, code, year, numIncidents
+        /**
+         * Returns the DisasterDescription instance of the most impactful type of disaster based on a specified year
+         * @param year The year to filter records by
+         * @param records The List of List of Strings containing the entries of disasters
+         * @return A DisasterDescription instance containing the category of the most impactful disaster during a specified year
+         */
 	public DisasterDescription getMostImpactfulDisasterByYear(String year, List<List<String>> records) {
 		// TODO implement this method
             DisasterDescription mostImpactfulDisasterDescription = new DisasterDescription(null, year, -1);
@@ -107,6 +142,12 @@ public class CodingCompCSVUtil {
             return mostImpactfulDisasterDescription;
 	}
 
+        /**
+         * Returns a DisasterDescription instance of the total reported incidents of a disaster category
+         * @param category The category to filter records by to find the total number of reported incidents
+         * @param records The List of List of Strings containing the disaster entries
+         * @return a DisasterDescription containing the total number of incidents from a specified category
+         */
 	public DisasterDescription getTotalReportedIncidentsByCategory(String category, List<List<String>> records) {
 		// TODO implement this method
             int totalForCategory = 0;
@@ -124,8 +165,11 @@ public class CodingCompCSVUtil {
 	 * 	+ A value of -1 is provided if the max range is NOT applicable.
 	 *  + A min value can be provided, without providing a max value (which then has to be -1 like indicated above).
 	 *  + If a max value is provided, then a max value is also needed.
+         *  @param records The List of List of Strings containing the disaster entries
+         *  @param min The minimum number of incidents to consider in the total (inclusive)
+         *  @param max The maximum number of incidents to consider in the total (inclusive) (Can be -1, which indicates no max and retrieve all values above minimum)
+         *  @return The count of impactful years within the specified range
 	 */
-        // entity, code, year, numIncidents
 	public int countImpactfulYearsWithReportedIncidentsWithinRange(List<List<String>> records, int min, int max) {
 		// TODO implement this method
             int total = 0;
@@ -146,6 +190,12 @@ public class CodingCompCSVUtil {
             return total;
 	}
 	
+        /**
+         * Returns whether the first records List contains more reported incidents than the second records List
+         * @param records1 The first List of List of Strings containing disaster entries
+         * @param records2 The second List of List of Strings continaining disaster entries
+         * @return A boolean of whether records1 contains more reported incidents than records2
+         */
 	public boolean firstRecordsHaveMoreReportedIndicents(List<List<String>> records1, List<List<String>> records2) {
 		// TODO implement this method
             return countImpactfulYearsWithReportedIncidentsWithinRange(records1, 1, -1) > countImpactfulYearsWithReportedIncidentsWithinRange(records2, 1, -1);
--- src/main/java/codingcompetition2019/DisasterDescription.java
+++ src/main/java/codingcompetition2019/DisasterDescription.java
@@ -1,23 +1,47 @@
 package codingcompetition2019;
 
+/**
+ * A class representing the DisasterDescription object
+ * @author Sidhartha Chaganti, schaganti@gatech.edu
+ * @version 1.0.0
+ */
 public class DisasterDescription {
-    // TODO finish this class
     private String year;
     private String category;
     private int reportedIncidentsNum;
 
+    /**
+     * Constructs a DisasterDescription object
+     * @param category The category of the disaster(s)
+     * @param year The year the disaster(s) took place
+     * @param reportedIncidentsNum The number of disasters of this type occured in this year
+     */
     public DisasterDescription(String category, String year, int reportedIncidentsNum) {
         this.year = year;
         this.category = category;
         this.reportedIncidentsNum = reportedIncidentsNum;
     }
 
+    /**
+     * Returns the year of this instance
+     * @return The year of this instance
+     */
     public String getYear() {
         return year;
     }
+
+    /**
+     * Returns the category of this instance
+     * @return The category of this instance
+     */
     public String getCategory() {
         return category;
     }
+
+    /**
+     * Returns the number of reported incidents
+     * @return The number of reported incidents
+     */
     public int getReportedIncidentsNum() {
         return reportedIncidentsNum;
     }
