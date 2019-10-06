d54ddd1d903b5555a63e50a27bc48c34782f0152
jmschonfeld <jeremyschonfeld@gmail.com>
2019-10-05 21:12:16 -0400

    Fill in disaster description

--- src/main/java/codingcompetition2019/DisasterDescription.java
+++ src/main/java/codingcompetition2019/DisasterDescription.java
@@ -1,5 +1,45 @@
 package codingcompetition2019;
 
 public class DisasterDescription {
-	// TODO finish this class
+	
+	private String entity, code;
+	private int year, statistic;
+	
+	public DisasterDescription(String[] data) {
+		if (data.length < 4) {
+			throw new IllegalArgumentException("Expected data to have at least 4 values");
+		}
+		
+		entity = data[0];
+		code = data[1];
+		try {
+			year = Integer.parseInt(data[2]);
+			statistic = Integer.parseInt(data[3]);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("Invalid year or statistic number", e);
+		}
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public String getCategory() {
+		return entity;
+	}
+	
+	public String getCountry() {
+		return entity;
+	}
+	
+	public int getReportedIncidentsNum() {
+		return statistic;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	
+	
 }
