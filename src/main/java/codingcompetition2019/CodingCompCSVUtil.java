f5f16979a21173c40b899793e8ba718fd7e5234b
Champ Foronda <champ.foronda@gmail.com>
2019-10-05 20:38:58 -0700

    Initial Commit

--- src/main/java/codingcompetition2019/CodingCompCSVUtil.java
+++ src/main/java/codingcompetition2019/CodingCompCSVUtil.java
@@ -2,11 +2,42 @@ package codingcompetition2019;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
-public class CodingCompCSVUtil {
-	public List<List<String>> readCSVFileByCountry(String fileName, String countryName) throws IOException {
+
+public class CodingCompCSVUtil 
+{
+	
+	private BufferedReader csvReader;
+	public List<List<String>> readCSVFileByCountry(String fileName, String countryName) throws IOException 
+	{	
 		// TODO implement this method
-		return null;
+		if(fileName.equals(""))
+		{
+			throw new IOException();
+		}
+		
+		List<List<String>> csvData = new ArrayList<List<String>>();
+		csvReader = new BufferedReader(new FileReader(fileName));
+		
+		String rowString = "";
+		while((rowString = csvReader.readLine()) != null)
+		{
+			String[] splitRowData = rowString.split(",");
+			if(splitRowData[0].equals(countryName))
+			{
+				List<String> rowEntryData = new ArrayList<String>();
+				for(int dataIndex = 0; dataIndex < splitRowData.length; dataIndex++)
+				{
+					rowEntryData.add(splitRowData[dataIndex]);
+				}
+				csvData.add(rowEntryData);
+			}
+		}
+		
+		return csvData;
 	}
 	
 	public List<List<String>> readCSVFileWithHeaders(String fileName) throws IOException {
@@ -14,9 +45,32 @@ public class CodingCompCSVUtil {
 		return null;
 	}
 	
-	public List<List<String>> readCSVFileWithoutHeaders(String fileName) throws IOException {
+	public List<List<String>> readCSVFileWithoutHeaders(String fileName) throws IOException 
+	{
 		// TODO implement this method
-		return null;
+		if(fileName.equals(""))
+		{
+			throw new IOException();
+		}
+		
+		csvReader = new BufferedReader(new FileReader(fileName));
+		List<List<String>> csvData = new ArrayList<List<String>>();
+		csvReader.readLine();
+		
+		String rowString = "";
+		while((rowString = csvReader.readLine()) != null)
+		{
+			String[] splitRowData = rowString.split(",");
+			List<String> rowEntryData = new ArrayList<String>();
+			for(int dataIndex = 0; dataIndex < splitRowData.length; dataIndex++)
+			{
+				rowEntryData.add(splitRowData[dataIndex]);
+			}
+			
+			csvData.add(rowEntryData);
+		}
+		
+		return csvData;
 	}
 	
 	public DisasterDescription getMostImpactfulYear(List<List<String>> records) {
@@ -24,9 +78,30 @@ public class CodingCompCSVUtil {
 		return null;
 	}
 
-	public DisasterDescription getMostImpactfulYearByCategory(String category, List<List<String>> records) {
+	public DisasterDescription getMostImpactfulYearByCategory(String category, List<List<String>> records) 
+	{
 		// TODO implement this method
-		return null;
+		DisasterDescription description = new DisasterDescription();
+		description.setCategory(category);
+		int maxImpact = 0;
+		int currentImpact = 0;
+		String maxYear = "";
+		
+		for(List<String> record : records)
+		{
+			currentImpact = Integer.valueOf(record.get(3));
+			if(record.get(0).equals(category))
+			{
+				if(currentImpact > maxImpact)
+				{
+					maxImpact = currentImpact;
+					maxYear = record.get(2);
+				}
+			}
+		}
+		
+		description.setYear(maxYear);
+		return description;
 	}
 
 	public DisasterDescription getMostImpactfulDisasterByYear(String year, List<List<String>> records) {
