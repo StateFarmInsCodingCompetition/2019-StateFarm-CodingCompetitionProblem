6084c67855e1495a53738323258a9cc965e842a6
jmschonfeld <jeremyschonfeld@gmail.com>
2019-10-05 21:18:26 -0400

    Change disaster description to use a list

--- src/main/java/codingcompetition2019/DisasterDescription.java
+++ src/main/java/codingcompetition2019/DisasterDescription.java
@@ -1,20 +1,22 @@
 package codingcompetition2019;
 
+import java.util.List;
+
 public class DisasterDescription {
 	
 	private String entity, code;
 	private int year, statistic;
 	
-	public DisasterDescription(String[] data) {
-		if (data.length < 4) {
+	public DisasterDescription(List<String> data) {
+		if (data.size() < 4) {
 			throw new IllegalArgumentException("Expected data to have at least 4 values");
 		}
 		
-		entity = data[0];
-		code = data[1];
+		entity = data.get(0);
+		code = data.get(1);
 		try {
-			year = Integer.parseInt(data[2]);
-			statistic = Integer.parseInt(data[3]);
+			year = Integer.parseInt(data.get(2));
+			statistic = Integer.parseInt(data.get(3));
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException("Invalid year or statistic number", e);
 		}
