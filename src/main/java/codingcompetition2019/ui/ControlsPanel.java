b6f57a2831eb836adc391b605c27a80cea2b5977
jmschonfeld <jeremyschonfeld@gmail.com>
2019-10-05 23:20:19 -0400

    Chart type selector

--- src/main/java/codingcompetition2019/ui/ControlsPanel.java
+++ src/main/java/codingcompetition2019/ui/ControlsPanel.java
@@ -1,13 +1,43 @@
 package codingcompetition2019.ui;
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ControlsPanel extends JPanel {
+public class ControlsPanel extends JPanel implements ItemListener {
 	private static final long serialVersionUID = 4645758996733040911L;
 	
 	private ChartView chart;
+	private ControlFilters filters;
+	
+	private JComboBox<String> chartTypeSelector;
 
 	public ControlsPanel(ChartView chart) {
 		this.chart = chart;
+		this.filters = new ControlFilters();
+		
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(BorderFactory.createTitledBorder("Options"));
+		JLabel typeLabel = new JLabel("Chart Type");
+		typeLabel.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+		this.add(typeLabel);
+		chartTypeSelector = new JComboBox<String>();
+		chartTypeSelector.addItem("Pie Chart");
+		chartTypeSelector.addItem("Bar Graph");
+		chartTypeSelector.addItem("Line Graph");
+		chartTypeSelector.addItemListener(this);
+		this.add(chartTypeSelector);
+	}
+
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			this.filters.setChartType(chartTypeSelector.getSelectedIndex());
+			this.chart.update(this.filters);
+		}
 	}
 }
