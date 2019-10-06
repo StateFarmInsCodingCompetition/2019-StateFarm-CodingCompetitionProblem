94b5eaf57d069a1c4f2fd73c6eedcfd2300c64ad
jmschonfeld <jeremyschonfeld@gmail.com>
2019-10-05 23:27:33 -0400

    Secondary parameter

--- src/main/java/codingcompetition2019/ui/ControlsPanel.java
+++ src/main/java/codingcompetition2019/ui/ControlsPanel.java
@@ -4,6 +4,7 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -15,7 +16,9 @@ public class ControlsPanel extends JPanel implements ItemListener {
 	private ChartView chart;
 	private ControlFilters filters;
 	
-	private JComboBox<String> chartTypeSelector;
+	private JComboBox<String> chartTypeSelector, xAxisSelector;
+	
+	private JLabel xAxisTitle;
 
 	public ControlsPanel(ChartView chart) {
 		this.chart = chart;
@@ -24,7 +27,7 @@ public class ControlsPanel extends JPanel implements ItemListener {
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.setBorder(BorderFactory.createTitledBorder("Options"));
 		JLabel typeLabel = new JLabel("Chart Type");
-		typeLabel.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+		typeLabel.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
 		this.add(typeLabel);
 		chartTypeSelector = new JComboBox<String>();
 		chartTypeSelector.addItem("Pie Chart");
@@ -32,11 +35,31 @@ public class ControlsPanel extends JPanel implements ItemListener {
 		chartTypeSelector.addItem("Line Graph");
 		chartTypeSelector.addItemListener(this);
 		this.add(chartTypeSelector);
+		
+		this.add(Box.createVerticalStrut(30));
+		
+		xAxisTitle = new JLabel("Slice By");
+		xAxisTitle.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
+		this.add(xAxisTitle);
+		xAxisSelector = new JComboBox<String>();
+		xAxisSelector.addItem("Year");
+		xAxisSelector.addItem("Category/Country");
+		xAxisSelector.addItemListener(this);
+		this.add(xAxisSelector);
 	}
 
 	public void itemStateChanged(ItemEvent e) {
 		if (e.getStateChange() == ItemEvent.SELECTED) {
-			this.filters.setChartType(chartTypeSelector.getSelectedIndex());
+			this.filters.setChartType(chartTypeSelector.getSelectedIndex() + 1);
+			this.filters.setXAxis(xAxisSelector.getSelectedIndex() + 1);
+			switch (chartTypeSelector.getSelectedIndex()) {
+			case 0:
+				xAxisTitle.setText("Slice By");
+				break;
+			default:
+				xAxisTitle.setText("X Axis");
+				break;
+			}
 			this.chart.update(this.filters);
 		}
 	}
