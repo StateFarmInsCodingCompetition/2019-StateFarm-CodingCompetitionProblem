b8360e225ecc6af7289715cfd9433a8b41ca223e
Ryan Thomas Lynch <hello@ryanlyn.ch>
2019-10-06 00:09:58 -0400

    created DisasterDataVisualizer

---
+++ src/main/java/codingcompetition2019/DisasterDataVisualizer.java
@@ -0,0 +1,13 @@
+package codingcompetition2019;
+
+import javax.swing.*;
+//TODO: fix the * import
+
+/**
+ * @author Mudit Gupta and Ryan Thomas Lynch
+ * @version 1.0
+ *
+ */
+public class DisasterDataVisualizer {
+
+}
