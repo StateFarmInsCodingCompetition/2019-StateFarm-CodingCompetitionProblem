758d282c5b35a6962dc3b913ad991194692b90a4
Ankur Sundara <me@ankursundara.com>
2019-10-05 23:55:03 -0500

    Finish

--- src/main/java/codingcompetition2019/DisasterDescription.java
+++ src/main/java/codingcompetition2019/DisasterDescription.java
@@ -1,5 +1,37 @@
 package codingcompetition2019;
 
+import java.util.List;
+
 public class DisasterDescription {
-	// TODO finish this class
+    private String category;
+    private String countryCode;
+    private String year;
+    private int numIncidents;
+
+    public DisasterDescription(String category, String countryCode, String year, int numIncidents) {
+        this.category = category;
+        this.countryCode = countryCode;
+        this.year = year;
+        this.numIncidents = numIncidents;
+    }
+
+    public DisasterDescription(List<String> csvRow) {
+        this(csvRow.get(0), csvRow.get(1), csvRow.get(2), Integer.parseInt(csvRow.get(3)));
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public String getCountryCode() {
+        return this.countryCode;
+    }
+
+    public String getYear() {
+        return this.year;
+    }
+
+    public int getReportedIncidentsNum() {
+        return this.numIncidents;
+    }
 }
---
+++ src/main/java/codingcompetition2019/webui/Main.java
@@ -0,0 +1,51 @@
+package codingcompetition2019.webui;
+
+import codingcompetition2019.CodingCompCSVUtil;
+import codingcompetition2019.DisasterDescription;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static spark.Spark.*;
+public class Main {
+    private static final String naturalDisasterByTypeFile = "src/main/resources/natural-disasters-by-type.csv";
+    private static final String significantEarthquakeFile = "src/main/resources/significant-earthquakes.csv";
+    private static final String significantVolanicEruptionsFile = "src/main/resources/significant-earthquakes.csv";
+
+    public static String loadJson(String fileName) throws IOException {
+        Gson gson = new Gson();
+        CodingCompCSVUtil util = new CodingCompCSVUtil();
+        List<List<String>> records = util.readCSVFileWithoutHeaders(fileName);
+        List<DisasterDescription> dds = records.stream().map(DisasterDescription::new).collect(Collectors.toList());
+        return gson.toJson(dds);
+    }
+
+    public static void main(String[] args) throws IOException {
+        String typeJson = loadJson(naturalDisasterByTypeFile);
+        String earthquakeJson = loadJson(significantEarthquakeFile);
+        String volcanoJson  = loadJson(significantVolanicEruptionsFile);
+
+
+        System.out.println("Running on: http://localhost:4567");
+
+        staticFiles.location("html");
+        //staticFiles.externalLocation("/home/hsheth/data/projects/2019-StateFarm-CodingCompetitionProblem-Private/src/main/resources/html");
+        get("/api/disasters_by_type", (req, res) -> {
+            res.type("application/json");
+            return typeJson;
+        });
+
+        get("/api/significant_earthquakes", (req, res) -> {
+            res.type("application/json");
+            return earthquakeJson;
+        });
+
+        get("/api/significant_volcanic_eruptions", (req, res) -> {
+            res.type("application/json");
+            return volcanoJson;
+        });
+    }
+
+}
