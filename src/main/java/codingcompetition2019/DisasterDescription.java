05a324cc2860853fb8d8a8bc81a8a6322104901b
rmitra34 <rmitra34@gatech.edu>
2019-10-05 22:17:57 -0400

    updated Disaster Description

--- src/main/java/codingcompetition2019/DisasterDescription.java
+++ src/main/java/codingcompetition2019/DisasterDescription.java
@@ -2,8 +2,19 @@ package codingcompetition2019;
 
 public class DisasterDescription {
 	private String year = "";
+	private String category = "";
+	private int numInYear = 0;
 	public DisasterDescription(String year) {
+	    this(year, "", 0);
+    }
+    public DisasterDescription(String year, String type, int num) {
 	    this.year = year;
+	    this.category = type;
+	    this.numInYear = num;
+    }
+
+    public int getReportedIncidentsNum() {
+	    return numInYear;
     }
 
     public String getYear() {
