1884721cb58fe2cfb790f18bb02951987d544d9c
Alex Kim <alexkimt@gmail.com>
2019-10-05 21:43:49 -0400

    Implemented the CSV reader methods:
    "readCSVFileByCountry", "readCSVFileWithHeaders",
    "readCSVFileWithoutHeaders"

--- src/main/java/codingcompetition2019/CodingCompCSVUtil.java
+++ src/main/java/codingcompetition2019/CodingCompCSVUtil.java
@@ -1,22 +1,81 @@
 package codingcompetition2019;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 
 public class CodingCompCSVUtil {
+	
+	
 	public List<List<String>> readCSVFileByCountry(String fileName, String countryName) throws IOException {
-		System.out.println("test");
-		return null;
+		List<List<String>> disasters = new ArrayList<List<String>>();
+		try {
+			String line;
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			while ((line = br.readLine()) != null) {
+				String[] lineArr = line.split(",");
+				if(lineArr[0].contentEquals(countryName)) { //country is equal
+					disasters.add(new ArrayList<String>()); //add new row
+					for(String str : lineArr) {
+						disasters.get(disasters.size()-1).add(str);
+					}
+				}
+			}
+			br.close();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		return disasters;
 	}
 	
 	public List<List<String>> readCSVFileWithHeaders(String fileName) throws IOException {
-		// TODO implement this method
-		return null;
+		
+		List<List<String>> disasters = new ArrayList<List<String>>();
+		try {
+			String line;
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			br.readLine(); //skip the header line
+			while ((line = br.readLine()) != null) {
+				String[] lineArr = line.split(",");
+				disasters.add(new ArrayList<String>()); //add new row
+				for(String str : lineArr) {
+					disasters.get(disasters.size()-1).add(str);
+				}
+				
+			}
+			br.close();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		return disasters;
 	}
 	
 	public List<List<String>> readCSVFileWithoutHeaders(String fileName) throws IOException {
-		// TODO implement this method
-		return null;
+		
+		List<List<String>> disasters = new ArrayList<List<String>>();
+		try {
+			String line;
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			while ((line = br.readLine()) != null) {
+				String[] lineArr = line.split(",");
+				disasters.add(new ArrayList<String>()); //add new row
+				for(String str : lineArr) {
+					disasters.get(disasters.size()-1).add(str);
+				}
+				
+			}
+			br.close();
+		}
+		catch (IOException e) {
+			throw e;
+		}
+		return disasters;
 	}
 	
 	public DisasterDescription getMostImpactfulYear(List<List<String>> records) {
@@ -54,5 +113,12 @@ public class CodingCompCSVUtil {
 	public boolean firstRecordsHaveMoreReportedIndicents(List<List<String>> records1, List<List<String>> records2) {
 		// TODO implement this method
 		return false;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		String significantEarthquakeFileNameName = "src/main/resources/significant-earthquakes.csv";
+		CodingCompCSVUtil util = new CodingCompCSVUtil();
+		
+		System.out.println(util.readCSVFileByCountry(significantEarthquakeFileNameName, "United States"));
 	}
 }
