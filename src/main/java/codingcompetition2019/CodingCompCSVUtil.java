25f9ee2a3b113ccedb93f77c45658499aefbd7c3
pokeflutist78770 <a.aguayo30@gmail.com>
2019-10-05 19:01:04 -0700

    Updated country data method

--- src/main/java/codingcompetition2019/CodingCompCSVUtil.java
+++ src/main/java/codingcompetition2019/CodingCompCSVUtil.java
@@ -1,12 +1,57 @@
 package codingcompetition2019;
 
+import java.io.File;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class CodingCompCSVUtil {
+	
+	
+	/**
+	 * 
+	 * @param fileName String representing the name of a csv file to be read
+	 * @param countryName String representing the name of a country 
+	 *                    to parse csv file data for
+	 * @return 2D List of Strings that serves as the data for a specific country.
+	 * @throws IOException
+	 */
 	public List<List<String>> readCSVFileByCountry(String fileName, String countryName) throws IOException {
-		// TODO implement this method
-		return null;
+		Scanner reader=null;
+		
+		//read in file
+		File newFile=new File(fileName);
+		try {
+			reader=new Scanner(newFile);
+		} catch (Exception e){
+			throw new IOException("File can not be read");
+		}
+		
+		List<List<String>> records=new ArrayList<>();
+		
+		//skip the header
+		reader.nextLine();
+		
+		//parse the file
+		while(reader.hasNextLine()) {
+			String newLine=reader.nextLine();
+			
+			//current line is a country record
+			if(newLine.startsWith(countryName)) {
+				ArrayList<String> separatedLine=
+						new ArrayList<String>(Arrays.asList(newLine.split(",")));
+				
+				records.add(separatedLine);
+			}
+		}
+		
+		reader.close();
+		
+		System.out.println(records.size());
+		return records;
 	}
 	
 	public List<List<String>> readCSVFileWithHeaders(String fileName) throws IOException {
--- src/main/java/codingcompetition2019/DisasterDescription.java
+++ src/main/java/codingcompetition2019/DisasterDescription.java
@@ -2,4 +2,10 @@ package codingcompetition2019;
 
 public class DisasterDescription {
 	// TODO finish this class
+	
+	//getYear
+	
+	//getAmtIncidents
+	
+	//getCAtegory
 }
