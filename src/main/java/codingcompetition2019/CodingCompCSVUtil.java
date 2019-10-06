330ff74cf866e9bde35db6b4c2d17b9998a95a75
Jamp-H <jmh824@nau.edu>
2019-10-05 20:38:05 -0700

    My portion of the test code

--- src/main/java/codingcompetition2019/CodingCompCSVUtil.java
+++ src/main/java/codingcompetition2019/CodingCompCSVUtil.java
@@ -1,27 +1,119 @@
 package codingcompetition2019;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class CodingCompCSVUtil {
+	private BufferedReader csvReader;
 	public List<List<String>> readCSVFileByCountry(String fileName, String countryName) throws IOException {
 		// TODO implement this method
-		return null;
+        if(fileName.equals(""))
+        {
+            throw new IOException();
+        }
+        
+        List<List<String>> csvData = new ArrayList<List<String>>();
+        
+        csvReader = new BufferedReader(new FileReader(fileName));
+        
+        String rowString = "";
+        while((rowString = csvReader.readLine()) != null)
+        {
+            String[] splitRowData = rowString.split(",");
+            if(splitRowData[0].equals(countryName))
+            {
+                List<String> rowEntryData = new ArrayList<String>();
+                for(int dataIndex = 0; dataIndex < splitRowData.length; dataIndex++)
+                {
+                    rowEntryData.add(splitRowData[dataIndex]);
+                }
+                csvData.add(rowEntryData);
+            }    
+        }
+        
+        return csvData;
 	}
 	
 	public List<List<String>> readCSVFileWithHeaders(String fileName) throws IOException {
-		// TODO implement this method
-		return null;
+		BufferedReader fileReader = null;
+		List<List<String>> CSVWithHeaders = new ArrayList<List<String>>();
+		List<String> rowData = new ArrayList<String>();
+		String[] rowHelper = new String[4];
+		try 
+		{
+			String line = "";
+			
+			// Creating file reader
+			fileReader = new BufferedReader(new FileReader(fileName));
+			
+			while((line = fileReader.readLine()) != null)
+			{
+				rowHelper = line.split(",");
+				for(int dataIndex = 0; dataIndex < rowHelper.length; dataIndex++)
+				{
+					rowData.add(rowHelper[dataIndex]);
+				}
+				CSVWithHeaders.add(rowData);
+			}
+			
+			fileReader.close();
+			return CSVWithHeaders;
+		}
+		
+		catch(Exception e)
+		{
+			throw new IOException();
+		}
 	}
 	
 	public List<List<String>> readCSVFileWithoutHeaders(String fileName) throws IOException {
-		// TODO implement this method
-		return null;
+		
+		if(fileName.equals(""))
+        {
+            throw new IOException();
+        }
+        
+        csvReader = new BufferedReader(new FileReader(fileName));
+        List<List<String>> csvData = new ArrayList<List<String>>();
+        csvReader.readLine();
+        
+        String rowString = "";
+        while((rowString = csvReader.readLine()) != null)
+        {
+            String[] splitRowData = rowString.split(",");
+            List<String> rowEntryData = new ArrayList<String>();
+            for(int dataIndex = 0; dataIndex < splitRowData.length; dataIndex++)
+            {
+                rowEntryData.add(splitRowData[dataIndex]);
+            }
+            
+            csvData.add(rowEntryData);
+        }
+        
+        return csvData;
 	}
 	
 	public DisasterDescription getMostImpactfulYear(List<List<String>> records) {
-		// TODO implement this method
-		return null;
+		int maxImpact = 0;
+		int currentImpact = 0;
+		String maxYear = "";
+		for(List<String> list:records)
+		{
+			currentImpact = Integer.valueOf(list.get(3));
+			if (currentImpact > maxImpact)
+			{
+				maxImpact = currentImpact;
+				maxYear = list.get(2);
+			}
+		}
+		DisasterDescription dd = new DisasterDescription();
+		dd.setYear(maxYear);
+		System.out.println(maxYear);
+		return dd;
 	}
 
 	public DisasterDescription getMostImpactfulYearByCategory(String category, List<List<String>> records) {
