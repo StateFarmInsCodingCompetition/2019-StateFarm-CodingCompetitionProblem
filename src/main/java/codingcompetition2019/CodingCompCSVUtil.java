2aec2d9edbad468a22a9357c0eb20d099a052081
Mudit Gupta <muditg317@gmail.com>
2019-10-05 21:34:53 -0400

    added in template javadocs to be filled in as completed

--- src/main/java/codingcompetition2019/CodingCompCSVUtil.java
+++ src/main/java/codingcompetition2019/CodingCompCSVUtil.java
@@ -4,6 +4,14 @@ import java.io.*;
 import java.util.*;
 
 public class CodingCompCSVUtil {
+
+    /**
+     * [readCSVFileByCountry description]
+     * @param  fileName    [description]
+     * @param  countryName [description]
+     * @return             [description]
+     * @throws IOException [description]
+     */
 	public List<List<String>> readCSVFileByCountry(String fileName, String countryName) throws IOException {
 		File file = new File(fileName);
 		FileReader fr = new FileReader(file);
@@ -18,37 +26,72 @@ public class CodingCompCSVUtil {
 		}
 		return interpretedFile;
 	}
-	
+
+    /**
+     * [readCSVFileWithHeaders description]
+     * @param  fileName    [description]
+     * @return             [description]
+     * @throws IOException [description]
+     */
 	public List<List<String>> readCSVFileWithHeaders(String fileName) throws IOException {
 		// TODO implement this method
 		return null;
 	}
-	
+
+    /**
+     * [readCSVFileWithoutHeaders description]
+     * @param  fileName    [description]
+     * @return             [description]
+     * @throws IOException [description]
+     */
 	public List<List<String>> readCSVFileWithoutHeaders(String fileName) throws IOException {
 		// TODO implement this method
 		return null;
 	}
-	
+
+    /**
+     * [getMostImpactfulYear description]
+     * @param  records [description]
+     * @return         [description]
+     */
 	public DisasterDescription getMostImpactfulYear(List<List<String>> records) {
 		// TODO implement this method
 		return null;
 	}
 
+    /**
+     * [getMostImpactfulYearByCategory description]
+     * @param  category [description]
+     * @param  records  [description]
+     * @return          [description]
+     */
 	public DisasterDescription getMostImpactfulYearByCategory(String category, List<List<String>> records) {
 		// TODO implement this method
 		return null;
 	}
 
+    /**
+     * [getMostImpactfulDisasterByYear description]
+     * @param  year    [description]
+     * @param  records [description]
+     * @return         [description]
+     */
 	public DisasterDescription getMostImpactfulDisasterByYear(String year, List<List<String>> records) {
 		// TODO implement this method
 		return null;
 	}
 
+    /**
+     * [getTotalReportedIncidentsByCategory description]
+     * @param  category [description]
+     * @param  records  [description]
+     * @return          [description]
+     */
 	public DisasterDescription getTotalReportedIncidentsByCategory(String category, List<List<String>> records) {
 		// TODO implement this method
 		return null;
 	}
-	
+
 	/**
 	 * This method will return the count if the number of incident falls within the provided range.
 	 * To simplify the problem, we assume:
@@ -60,7 +103,13 @@ public class CodingCompCSVUtil {
 		// TODO implement this method
 		return -1;
 	}
-	
+
+    /**
+     * [firstRecordsHaveMoreReportedIndicents description]
+     * @param  records1 [description]
+     * @param  records2 [description]
+     * @return          [description]
+     */
 	public boolean firstRecordsHaveMoreReportedIndicents(List<List<String>> records1, List<List<String>> records2) {
 		// TODO implement this method
 		return false;
--- src/main/java/codingcompetition2019/DisasterDescription.java
+++ src/main/java/codingcompetition2019/DisasterDescription.java
@@ -1,14 +1,26 @@
 package codingcompetition2019;
 
 public class DisasterDescription {
-	public String getCategory() {
+    /**
+     * [getCategory description]
+     * @return [description]
+     */
+    public String getCategory() {
 		return null;
 	}
-	
+
+    /**
+     * [getReportedIncidentsNum description]
+     * @return [description]
+     */
 	public int getReportedIncidentsNum() {
 		return -1;
 	}
-	
+
+    /**
+     * [getYear description]
+     * @return [description]
+     */
 	public int getYear() {
 		return -1;
 	}
